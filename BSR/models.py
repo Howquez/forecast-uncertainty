17a51bc9ef90c37597843890d12b3e4904fc03d3
howquez <hauke2roggenkamp@gmail.com>
2020-09-04 16:46:00 +0200

    adding payoff function for BSR

--- BSR/models.py
+++ BSR/models.py
@@ -11,6 +11,10 @@ from otree.api import (
 
 from otree.db.models import Model, ForeignKey
 
+from random import randint, seed
+
+seed(123456)
+
 
 author = 'Hauke Roggenkamp'
 
@@ -23,7 +27,9 @@ class Constants(BaseConstants):
     name_in_url = 'BSR'
     players_per_group = None
     num_rounds = 1
-    temp_range = [-9, 40]
+    BSR_prize = [0, 10]
+    observed_temp = 25
+    temp_range = [0, 50]
 
 
 class Subsession(BaseSubsession):
@@ -39,6 +45,7 @@ class Player(BasePlayer):
     minTemp=models.IntegerField(
         label="Was ist die niedrigste Temperatur, die Ihrer Meinung nach gerade so noch wahrscheinlich ist?",
         min=Constants.temp_range[0],
+        initial=24,
         # max=Constants.temp_range[1],
         doc="define the range of temperatures with positive probability"
     )
@@ -49,6 +56,7 @@ class Player(BasePlayer):
     maxTemp = models.IntegerField(
         label="Was ist die höchste Temperatur, die Ihrer Meinung nach gerade so noch wahrscheinlich ist?",
         # min=Constants.temp_range[0],
+        initial=26,
         max=Constants.temp_range[1],
         doc="define the range of temperatures with positive probability"
     )
@@ -56,19 +64,40 @@ class Player(BasePlayer):
     def maxTemp_min(self):
         return self.minTemp
 
-    temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9, temp10,\
-        temp11, temp12, temp13, temp14, temp15, temp16, temp17, temp18, temp19, temp20,\
-        temp21, temp22, temp23, temp24, temp25, temp26, temp27, temp28, temp29, temp30,\
-        temp31, temp32, temp33, temp34, temp35, temp36, temp37, temp38, temp39, temp40,\
-        temp41, temp42, temp43, temp44, temp45, temp46, temp47, temp48, temp49, temp50, temp51,\
+    prob0, prob1, prob2, prob3, prob4, prob5, prob6, prob7, prob8, prob9, prob10,\
+        prob11, prob12, prob13, prob14, prob15, prob16, prob17, prob18, prob19, prob20,\
+        prob21, prob22, prob23, prob24, prob25, prob26, prob27, prob28, prob29, prob30,\
+        prob31, prob32, prob33, prob34, prob35, prob36, prob37, prob38, prob39, prob40,\
+        prob41, prob42, prob43, prob44, prob45, prob46, prob47, prob48, prob49, prob50,\
         = [models.IntegerField(min=0,
                                max=100,
                                label="{}°C".format(i),
-                               doc="{}°C".format(i),
+                               doc="probability assigned to {}°C".format(i),
                                blank=True,
                                initial=0)
-    for i in range(Constants.temp_range[0]-1,
-                   Constants.temp_range[1]+1)]
+    for i in range(Constants.temp_range[0],
+                   Constants.temp_range[1] + 1)]
+
+    random_int = models.IntegerField()
+    loss = models.CurrencyField()
+
+    def set_loss(self):
+        self.random_int = randint(0, 100)
+        self.loss = 0
+        for i in range(Constants.temp_range[1] + abs(Constants.temp_range[0] + 1)): #0<=i<=49+1
+            p = eval("self.prob{}".format(i))/100
+            if i == Constants.observed_temp + abs(Constants.temp_range[0]):
+                l=((1-p)**2)*100
+            else:
+                l=(p**2)*100
+            self.loss+=l
+
+    def set_payoff(self):
+        if self.loss <= self.random_int:
+            self.payoff = Constants.BSR_prize[1]
+        else:
+            self.payoff = Constants.BSR_prize[0]
+
 
 
 
--- BSR/pages.py
+++ BSR/pages.py
@@ -20,14 +20,18 @@ class BSR(Page):
     # def get_form_fields(self):
     #     return ["temp{}".format(i) for i in range(self.player.minTemp, self.player.maxTemp+1)]
 
-    form_fields=["temp1", "temp2", "temp3", "temp4", "temp5", "temp6", "temp7", "temp8", "temp9", "temp10",\
-        "temp11", "temp12", "temp13", "temp14", "temp15", "temp16", "temp17", "temp18", "temp19", "temp20",\
-        "temp21", "temp22", "temp23", "temp24", "temp25", "temp26", "temp27", "temp28", "temp29", "temp30",\
-        "temp31", "temp32", "temp33", "temp34", "temp35", "temp36", "temp37", "temp38", "temp39", "temp40",\
-        "temp41", "temp42", "temp43", "temp44", "temp45", "temp46", "temp47", "temp48", "temp49", "temp50", "temp51"]
+    form_fields=["prob0", "prob1", "prob2", "prob3", "prob4", "prob5", "prob6", "prob7", "prob8", "prob9", "prob10",\
+        "prob11", "prob12", "prob13", "prob14", "prob15", "prob16", "prob17", "prob18", "prob19", "prob20",\
+        "prob21", "prob22", "prob23", "prob24", "prob25", "prob26", "prob27", "prob28", "prob29", "prob30",\
+        "prob31", "prob32", "prob33", "prob34", "prob35", "prob36", "prob37", "prob38", "prob39", "prob40",\
+        "prob41", "prob42", "prob43", "prob44", "prob45", "prob46", "prob47", "prob48", "prob49", "prob50"]
 
     def get_form_fields(self):
-        return self.form_fields[self.player.minTemp + 10 : self.player.maxTemp + 11]
+        return self.form_fields[self.player.minTemp : self.player.maxTemp + 1]
+
+    def before_next_page(self):
+        self.player.set_loss()
+        self.player.set_payoff()
     pass
 
 class ResultsWaitPage(WaitPage):
@@ -35,11 +39,12 @@ class ResultsWaitPage(WaitPage):
 
 
 class Results(Page):
+
     pass
 
 
 page_sequence = [# Instructions,
                  BSR_PreSelection,
                  BSR,
-                 ResultsWaitPage,
+                 # ResultsWaitPage,
                  Results]
