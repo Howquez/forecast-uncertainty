c38a673b1633ae7692583dbf89e3e2ca04cc892f
Hauke Roggenkamp <hauke.roggenkamp@sinnerschrader.com>
2020-10-27 12:56:59 +0100

    add weather data & improve viz

--- BSR/static/BSR/js/revelation_viz.js
+++ BSR/static/BSR/js/revelation_viz.js
@@ -1,6 +1,5 @@
 // read data
-// https://kachelmannwetter.com/de/messwerte/63-e-491-n/temperatur/20201009-1200z.html#obs-detail-J728-72h
-var weiskirchenRanges = [
+var weiskirchenRange = [
         [1601992800000, null, null],
         [1602079200000, null, null],
         [1602165600000, null, null],
@@ -14,6 +13,23 @@ var weiskirchenRanges = [
         [1602856800000, null, null],
         [1602943200000, null, null],
         [1603029600000, 7.6, 13.8], //20201018 to be displayed
+        [1603116000000, null, null],
+    ];
+
+var ilomantsiRange = [
+        [1601992800000, null, null],
+        [1602079200000, null, null],
+        [1602165600000, null, null],
+        [1602252000000, null, null],
+        [1602338400000, null, null],
+        [1602424800000, null, null],
+        [1602511200000, null, null],
+        [1602597600000, null, null],
+        [1602684000000, null, null],
+        [1602770400000, null, null],
+        [1602856800000, null, null],
+        [1602943200000, null, null],
+        [1603029600000, 2.0, 8.0], //20201018 to be displayed
         [1603116000000, null, null],
     ];
 
@@ -34,6 +50,25 @@ var weiskirchenBestGuess = [
         [1603116000000, null],
     ];
 
+var ilomantsiBestGuess = [
+        [1601992800000, null],
+        [1602079200000, null],
+        [1602165600000, null],
+        [1602252000000, null],
+        [1602338400000, null],
+        [1602424800000, null],
+        [1602511200000, null],
+        [1602597600000, null],
+        [1602684000000, null],
+        [1602770400000, null],
+        [1602856800000, null],
+        [1602943200000, null],
+        [1603029600000, 4.2], //20201018 to be displayed
+        [1603116000000, null],
+    ];
+
+// https://kachelmannwetter.com/de/messwerte/63-e-491-n/temperatur/20201009-1100z.html#obs-detail-J728-72h
+// https://kachelmannwetter.com/de/messwerte/63-e-491-n/temperatur/20201019-1100z.html#obs-detail-J728-72h
 var weiskirchenObserved = [
         [1601992800000, null], //20201006
         [1602079200000, 11.8], //20201007 to be displayed
@@ -51,12 +86,52 @@ var weiskirchenObserved = [
         [1603116000000, null], //20201019
     ];
 
+// https://kachelmannwetter.com/de/messwerte/ostfinnland/temperatur/20201019-1100z.html#obs-detail-029390-72h
+// https://kachelmannwetter.com/de/messwerte/ostfinnland/temperatur/20201009-1100z.html#obs-detail-029390-72h
+var ilomantsiObserved = [
+        [1601992800000, null], //20201006
+        [1602079200000, 12.0], //20201007 to be displayed
+        [1602165600000, 11.9], //20201008 to be displayed
+        [1602252000000, 12.1], //20201009 to be displayed
+        [1602338400000, 10.1], //20201010
+        [1602424800000, 9.1],  //20201011
+        [1602511200000, 8.9],  //20201012
+        [1602597600000, 8.0],  //20201013
+        [1602684000000, 5.9], //20201014
+        [1602770400000, 3.4],  //20201015
+        [1602856800000, 1.0],  //20201016
+        [1602943200000, 4.1],  //20201017
+        [1603029600000, 3.2],  //20201018 to be revealed
+        [1603116000000, null], //20201019
+    ];
+
 // get vars from python
-// let location = js_vars.location;   // equals "Weiskirchen" or "Ilomantsi"
+let village = js_vars.location;   // equals "Weiskirchen" or "Ilomantsi"
 let treatment = js_vars.treatment; // equals "best_guess" or "interval"
 
-// other variables
+// page and treatment specific operations
 var displayForecast = false;
+var observed;
+var bestGuess;
+var Range;
+
+if (village == "Ilomantsi"){
+    observed = ilomantsiObserved;
+    bestGuess = ilomantsiBestGuess;
+    range = ilomantsiRange;
+} else if (village == "Weiskirchen"){
+    observed = weiskirchenObserved;
+    bestGuess = weiskirchenBestGuess;
+    range = weiskirchenRange;
+}
+
+// text operations
+function fillText() {
+
+    document.getElementById("revelation").innerHTML = observed[12][1];
+    document.getElementById("village").innerHTML = village;
+
+    }
 
 // color definitions
 var opacity = 0.1;
@@ -69,7 +144,9 @@ var forecastRGBA  = `rgba(197, 130, 255, ${opacity*1.5})`;
 // language plot options
 Highcharts.setOptions({
     lang: {
-        weekdays: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+        months:        ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+        shortMonths:   ["Jan.", "Feb.", "März", "Apr.", "Mai", "Jun.", "Jul.", "Aug.", "Sept.", "Okt.", "Nov.", "Dez."],
+        weekdays:      ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
         shortWeekdays: ['So.', 'Mo.', 'Di.', 'Mi.', 'Do.', 'Fr.', 'Sa.']
     },
 });
@@ -80,6 +157,14 @@ var chart = Highcharts.chart('container', {
         enabled: false
     },
 
+    plotOptions: {
+        series: {
+            animation:{
+                duration: 2000
+            }
+        }
+    },
+
     title: {
         text: ""
     },
@@ -88,12 +173,6 @@ var chart = Highcharts.chart('container', {
         type: "datetime",
         accessibility: {
             rangeDescription: ""
-        },
-        labels: {
-            formatter: function() {
-            var dayStr = Highcharts.dateFormat('%a', this.value);
-            return dayStr;
-            }
         },
         plotBands: [{
         from: 1602079200000 - 43200000,
@@ -115,7 +194,7 @@ var chart = Highcharts.chart('container', {
     },
     yAxis: {
         title: {
-            text: null
+            text: "Temperatur in °C"
         },
         min: 0,
         max: 25,
@@ -124,13 +203,12 @@ var chart = Highcharts.chart('container', {
     tooltip: {
         crosshairs: true,
         shared: true,
-        valueSuffix: "°C",
-        xDateFormat: "%A"
+        valueSuffix: "°C"
     },
 
     series: [{
         name: "Beobachtete Temperatur in °C",
-        data: weiskirchenObserved,
+        data: observed,
         zIndex: 1,
         color: observedColor,//Highcharts.getOptions().colors[0]
         marker: {
@@ -140,8 +218,8 @@ var chart = Highcharts.chart('container', {
         },
         showInLegend: displayForecast,
     }, {
-        name: "Best Guess",
-        data: weiskirchenBestGuess,
+        name: "Wettervorhersage",
+        data: bestGuess,
         zIndex: 1,
         color: forecastColor,
         marker: {
@@ -153,7 +231,7 @@ var chart = Highcharts.chart('container', {
         visible: displayForecast
     }, {
         name: "Wahrscheinlichster Bereich",
-        data: weiskirchenRanges,
+        data: range,
         type: "arearange",
         lineWidth: 0,
         linkedTo: ":previous",
--- Baillon/static/Baillon/js/weather_viz.js
+++ Baillon/static/Baillon/js/weather_viz.js
@@ -1,6 +1,5 @@
 // read data
-// https://kachelmannwetter.com/de/messwerte/63-e-491-n/temperatur/20201009-1200z.html#obs-detail-J728-72h
-var weiskirchenRanges = [
+var weiskirchenRange = [
         [1601992800000, null, null],
         [1602079200000, null, null],
         [1602165600000, null, null],
@@ -14,6 +13,23 @@ var weiskirchenRanges = [
         [1602856800000, null, null],
         [1602943200000, null, null],
         [1603029600000, 7.6, 13.8], //20201018 to be displayed
+        [1603116000000, null, null],
+    ];
+
+var ilomantsiRange = [
+        [1601992800000, null, null],
+        [1602079200000, null, null],
+        [1602165600000, null, null],
+        [1602252000000, null, null],
+        [1602338400000, null, null],
+        [1602424800000, null, null],
+        [1602511200000, null, null],
+        [1602597600000, null, null],
+        [1602684000000, null, null],
+        [1602770400000, null, null],
+        [1602856800000, null, null],
+        [1602943200000, null, null],
+        [1603029600000, 2.0, 8.0], //20201018 to be displayed
         [1603116000000, null, null],
     ];
 
@@ -34,6 +50,25 @@ var weiskirchenBestGuess = [
         [1603116000000, null],
     ];
 
+var ilomantsiBestGuess = [
+        [1601992800000, null],
+        [1602079200000, null],
+        [1602165600000, null],
+        [1602252000000, null],
+        [1602338400000, null],
+        [1602424800000, null],
+        [1602511200000, null],
+        [1602597600000, null],
+        [1602684000000, null],
+        [1602770400000, null],
+        [1602856800000, null],
+        [1602943200000, null],
+        [1603029600000, 4.2], //20201018 to be displayed
+        [1603116000000, null],
+    ];
+
+// https://kachelmannwetter.com/de/messwerte/63-e-491-n/temperatur/20201009-1100z.html#obs-detail-J728-72h
+// https://kachelmannwetter.com/de/messwerte/63-e-491-n/temperatur/20201019-1100z.html#obs-detail-J728-72h
 var weiskirchenObserved = [
         [1601992800000, null], //20201006
         [1602079200000, 11.8], //20201007 to be displayed
@@ -51,25 +86,66 @@ var weiskirchenObserved = [
         [1603116000000, null], //20201019
     ];
 
+// https://kachelmannwetter.com/de/messwerte/ostfinnland/temperatur/20201019-1100z.html#obs-detail-029390-72h
+// https://kachelmannwetter.com/de/messwerte/ostfinnland/temperatur/20201009-1100z.html#obs-detail-029390-72h
+var ilomantsiObserved = [
+        [1601992800000, null], //20201006
+        [1602079200000, 12.0], //20201007 to be displayed
+        [1602165600000, 11.9], //20201008 to be displayed
+        [1602252000000, 12.1], //20201009 to be displayed
+        [1602338400000, 10.1], //20201010
+        [1602424800000, 9.1],  //20201011
+        [1602511200000, 8.9],  //20201012
+        [1602597600000, 8.0],  //20201013
+        [1602684000000, 5.9], //20201014
+        [1602770400000, 3.4],  //20201015
+        [1602856800000, 1.0],  //20201016
+        [1602943200000, 4.1],  //20201017
+        [1603029600000, 3.2],  //20201018 to be revealed
+        [1603116000000, null], //20201019
+    ];
+
 // get vars from python
-// let location = js_vars.location;   // equals "Weiskirchen" or "Ilomantsi"
+let village = js_vars.location;   // equals "Weiskirchen" or "Ilomantsi"
 let treatment = js_vars.treatment; // equals "best_guess" or "interval"
 let page = js_vars.page; // equals "historic" or "forecast" or "revelation"
 
-
 // page and treatment specific operations
 var displayForecast = false;
+var observed;
+var bestGuess;
+var Range;
 
+if (village == "Ilomantsi"){
+    observed = ilomantsiObserved;
+    bestGuess = ilomantsiBestGuess;
+    range = ilomantsiRange;
+} else if (village == "Weiskirchen"){
+    observed = weiskirchenObserved;
+    bestGuess = weiskirchenBestGuess;
+    range = weiskirchenRange;
+}
+
+// text operations
+function fillText() {
+    var reportArray = [observed[1][1],
+    observed[2][1],
+    observed[3][1]];
+
+    document.getElementById("report0").innerHTML = reportArray[0];
+    document.getElementById("report1").innerHTML = reportArray[1];
+    document.getElementById("report2").innerHTML = reportArray[2];
+    }
 
 // display forecast after the first round, i.e. in postBaillon App
 if (page == "forecast"){
     displayForecast = true;
 }
 
-for (var i = 0; i < weiskirchenObserved.length; ++i){
+for (var i = 0; i < observed.length; ++i){
     if (page != "revelation"){
         if (i > 3){
-            weiskirchenObserved[i][1] = null;
+            observed[i][1] = null;
         }
     }
 }
@@ -131,7 +207,7 @@ var chart = Highcharts.chart('container', {
     },
     yAxis: {
         title: {
-            text: null
+            text: "Temperatur in °C"
         },
         min: 0,
         max: 25,
@@ -146,7 +222,7 @@ var chart = Highcharts.chart('container', {
 
     series: [{
         name: "Beobachtete Temperatur in °C",
-        data: weiskirchenObserved,
+        data: observed,
         zIndex: 1,
         color: observedColor,//Highcharts.getOptions().colors[0]
         marker: {
@@ -156,8 +232,8 @@ var chart = Highcharts.chart('container', {
         },
         showInLegend: displayForecast,
     }, {
-        name: "Best Guess",
-        data: weiskirchenBestGuess,
+        name: "Wettervorhersage",
+        data: bestGuess,
         zIndex: 1,
         color: forecastColor,
         marker: {
@@ -169,7 +245,7 @@ var chart = Highcharts.chart('container', {
         visible: displayForecast
     }, {
         name: "Wahrscheinlichster Bereich",
-        data: weiskirchenRanges,
+        data: range,
         type: "arearange",
         lineWidth: 0,
         linkedTo: ":previous",
