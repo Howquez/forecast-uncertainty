3e480e110bc32ad316bec8606a8b3f0724a031bd
Hauke Roggenkamp <hauke.roggenkamp@sinnerschrader.com>
2020-11-05 22:08:43 +0100

    Add or improve instructions

--- initialize/pages.py
+++ initialize/pages.py
@@ -2,12 +2,11 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
+class Intro_Welcome(Page):
+    pass
 
 class Intro_Instructions(Page):
     pass
 
-class Intro_Welcome(Page):
-    pass
 
-
-page_sequence = [Intro_Welcome]
+page_sequence = [Intro_Welcome, Intro_Instructions]
