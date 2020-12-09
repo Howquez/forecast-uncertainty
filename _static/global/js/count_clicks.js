3e100b95765c2b1e6281ec8a7283d6384c626142
Hauke Roggenkamp <hauke.roggenkamp@sinnerschrader.com>
2020-12-09 22:06:56 +0100

    enable click tracking

---
+++ _static/global/js/count_clicks.js
@@ -0,0 +1,26 @@
+console.log("engines running: clicktracking");
+
+function countClicks(button){
+	var element = "id_review_" + button
+	var oldValue = parseInt(document.getElementById(element).value);
+	var newValue = oldValue+1;
+	document.getElementById(element).value = newValue;
+}
+
+// document.getElementById("weather_icon").addEventListener("click", function() {
+//     var element = "id_review_weather";
+//     console.log(element);
+// 	var oldValue = parseInt(document.getElementById(element).value);
+// 	var newValue = oldValue+1;
+// 	console.log(newValue);
+// 	document.getElementById(element).value = newValue;
+// } false);
+
+
+document.getElementById("weather_icon").addEventListener("click", function() {
+    countClicks("weather");
+}, false);
+
+document.getElementById("instructions_icon").addEventListener("click", function() {
+    countClicks("instructions");
+}, false);
