24237152721be02c7a588b1618ebbde814a16400
Hauke Roggenkamp <hauke.roggenkamp@sinnerschrader.com>
2021-01-20 14:45:07 +0100

    trigger modal onload

---
+++ oTree/_static/global/js/trigger_modal.js
@@ -0,0 +1,15 @@
+console.log("trigger")
+
+var weatherModal = document.getElementById("weatherModal")
+
+// document.addEventListener('DOMContentLoaded', function() {
+//    $(weatherModal).modal('show')
+// }, false);
+
+// window.onload = function trigger(){
+// 	$(weatherModal).modal('show')
+// }
+
+$(document).ready(function(){
+	$(weatherModal).modal('show')
+});
